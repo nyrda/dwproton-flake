37ed3c85c2c7593d181656505ac73b56446f121d
Momoyaan <89137647+Momoyaan@users.noreply.github.com>
2026-04-27 03:31:13 +0000

    Update dwproton to 10.0-26

--- default.nix
+++ default.nix
@@ -5,11 +5,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "dwproton";
-  version = "10.0-24";
+  version = "10.0-26";
 
   src = fetchzip {
     url = "https://dawn.wine/dawn-winery/dwproton/releases/download/dwproton-${finalAttrs.version}/dwproton-${finalAttrs.version}-x86_64.tar.xz";
-    hash = "sha256-3mfJGi2pUwPgWNZCvGD1SNHghS2HThX5Y7TrnJaEYvw=";
+    hash = "sha256-TkwhJCHPS0PdDIEL5GrxJPR09uO9U2DR8l9KWFLIF2g=";
   };
 
   dontUnpack = true;
